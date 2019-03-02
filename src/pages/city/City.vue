6f268ae85a54c3689ea585a789499435b18e911b
limsanity <798607646@qq.com>
2019-03-02 21:23:41 +0800

    performance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,14 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list
+      :cities="cities"
+      :hot="hotCities"
+      :letter="letter"></city-list>
+    <city-alphabet
+      :cities="cities"
+      @change="handleLetterChange"
+    ></city-alphabet>
   </div>
 </template>
 
@@ -23,10 +29,9 @@ export default {
   },
   data () {
     return {
-      cities: {
-
-      },
-      hotCities: []
+      cities: {},
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -42,6 +47,9 @@ export default {
         this.hotCities = data.hotCities
       }
       console.log(res)
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   },
   mounted () {
