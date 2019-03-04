d31842b0f28ff0b8b50812e12605c48785ec657c
limsanity <798607646@qq.com>
2019-03-04 22:24:50 +0800

    banner finish

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container" @click="handleGalleryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(item, index) in imgs" :key="index">
+          <img class="gallery-img" :src="item">
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallery',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return [
+          'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg',
+          'http://img1.qunarzz.com/sight/p55/201211/04/fbcab3e5d6479ce893835fbb.jpg_350x240_df8ba9f7.jpg'
+        ]
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGalleryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow inherit
+  .container
+    display flex
+    flex-direction column
+    justify-content center
+    z-index 99
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background #000
+    .wrapper
+      // overflow hidden
+      height 0
+      width 100%
+      padding-bottom 100%
+      .gallery-img
+        width 100%
+      .swiper-pagination
+        color #fff
+        bottom -1rem
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-title">故宫(AAAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe67b;</span>
+          8
+        </div>
+      </div>
+    </div>
+    <common-gallery
+      :imgs="imgs"
+      v-show="showGallary"
+      @close="handleGalleryClose"
+    ></common-gallery>
+  </div>
+
+</template>
+
+<script>
+import CommonGallery from 'common/gallery/Gallery'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg',
+        'http://img1.qunarzz.com/sight/p55/201211/04/fbcab3e5d6479ce893835fbb.jpg_350x240_df8ba9f7.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGalleryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallery
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #fff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        height .4rem
+        padding 0 .4rem
+        line-height .4rem
+        margin-top .14rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+</style>
